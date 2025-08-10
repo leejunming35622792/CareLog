f89fe5f8b976f6f99f18079821f5e0cc7ecaffd7
leejunming35622792 <jlee0465@student.monash.edu>
2025-08-10 14:18:08 +0800

    Update Changes - Comment

--- msms-project/msms.py
+++ msms-project/msms.py
@@ -8,7 +8,7 @@ class Student:  #to create Student object
         self.name = name
         self.enrolled_in = [] #to store students instruments
 
-class Teacher:  #to create Teacher object
+class Teacher:  # to create Teacher object
     def __init__(self, teacher_id, name, speciality):
         self.id = teacher_id
         self.name = name
@@ -19,78 +19,83 @@ class Teacher:  #to create Teacher object
 student_db = []
 teacher_db = []
 
-#starting of student/teacher id
+# starting of student/teacher id
 next_student_id = 1
 next_teacher_id = 1
 
-admin_pw = "admin1234" #password to unlock system
-staff_pw = "staff1234" #password access to view all students/teachers
+admin_pw = "admin1234" # password to unlock system
+staff_pw = "staff1234" # password access to view all students/teachers
 
 
 # --- Core Helper Functions ---
-#to add new students
+# to add new students
 def add_student():
+    # variable
     all_name = []
 
     while True:
-        #case handling
+        # case handling
         try:
+            # remove whitespace after words and capitalise every first letter
             name = input("Enter student name: ").rstrip().title()
 
-            #remove all whitespaces
+            # remove all whitespaces
             checkName = name.replace(" ","")
 
-            #check if input is all letters
+            # check if input is all letters
             if checkName.isalpha () == False: 
                 raise ValueError
             break
 
         except ValueError:
-            print(f"Please enter a valid name!\n")
+            print("Please enter a valid name!\n")
         
     while True:
-        #case handling
+        # case handling
         try:
             instrument = input("Enter instrument to enrol in: ").title()
             
-            #remove all whitespaces
+            # remove all whitespaces
             checkInstrument = instrument.replace(" ","")
 
-            #check if input is all letters
+            # check if input is all letters
             if checkInstrument.isalpha() != True: 
                 raise ValueError
             break
 
         except ValueError:
-            print(f"Please enter a valid instrument!\n")
+            print("Please enter a valid instrument!\n")
 
+    # generate a list with all student names
     for student in student_db:
         all_name.append(student.name)
 
     if name in all_name:
         print(f"\nAlert - '{student.name}' with '{student.enrolled_in} has been recorded under ID '{student.id}' before.'") 
 
-        print(f"1 - Add new student\n2 - Exit and Enroll existing student")
+        print("1 - Add new student")
+        print("2 - Exit and Enroll existing student")
 
         while True:
             proceed = input("Choose (1/2): ")
 
             if proceed == "1":
-                #return name and instrument with first letter of every word in capital
+                # return name and instrument
                 return name, instrument
         
+            # return to main()
             elif proceed == "2":
                 return main()
 
             else:
-                print(f"Please enter 1 or 2 only.\n")
+                print("Please enter 1 or 2 only.\n")
 
     else:
         return name, instrument
     
-#to add new teachers
+# to add new teachers
 def add_teacher(name, speciality):
-    # variables
+    # variable
     global next_teacher_id
 
     # generate a list of all teacher's name
@@ -102,18 +107,21 @@ def add_teacher(name, speciality):
     # to check whether a teacher has registered before
     if name in all_name:
         print(f"Teacher '{name}' is already recorded under '{speciality}' speciality.")
+
         while True:
             print("1 - Add new teacher")
             print("2 - Change existing data")
             print("3 - Exit")
             proceed = input("Choose (1/2/3): ")
 
+            # add new teacher
             if proceed == "1":
                 teacher_db.append(new_teacher)
                 next_teacher_id += 1
                 print(f"Core: Teacher '{name}' with speciality '{speciality}' added successfully.")
                 return 
 
+            # change the existing data
             elif proceed == "2":
                 for teacher in teacher_db:
                     if teacher.name == name:
@@ -121,77 +129,80 @@ def add_teacher(name, speciality):
                         print(f"Update '{teacher.name}' to speciality '{teacher.speciality}'.")
                 return
             
+            # return to main()
             elif proceed == "3":
-                return  # Exit function
+                return
 
             else:
                 print("Please enter 1, 2, or 3 only.")
 
-    # If teacher does not exist, add them
+    # add new teacher, increase id by 1
     teacher_db.append(new_teacher)
     next_teacher_id += 1
     print(f"Core: Teacher '{name}' with speciality '{speciality}' added successfully.")
     
-#to list all students
+# to list all students
 def list_students():
     print("\n--- Student List ---")
 
-    #if student_db has no element
+    # if student_db has no element
     if not student_db:
         print("No students in the system.")
         return
     
-    #loop student_db, and print every element in a particular format
+    # loop student_db, and print every element in a particular format
     for student in student_db:
         print(f"  ID: {str(student.id).ljust(10)}, Name: {str(student.name).ljust(15)}, Enrolled in: {str(student.enrolled_in).ljust(10)}")
 
-#to list all teachers
+# to list all teachers
 def list_teachers():
     print("\n--- Teacher List ---")
 
-    #if teacher_db has no element
+    # if teacher_db has no element
     if not student_db:
-        print(f"No teachers in the system.")
+        print("No teachers in the system.")
         return
 
-    #loop teacher_db, and print every element in a particular format
+    # loop teacher_db, and print every element in a particular format
     for teacher in teacher_db:
         print(f"  ID: {str(teacher.id).ljust(10)}, Name: {str(teacher.name).ljust(15)}, Speciality: {str(teacher.speciality).ljust(10)}")
 
-#to find student by name
-#case sensitive
+# to find student by name
+# case sensitive
 def find_students(term):
-    #variables
+    # variable
     global name_found
 
     # generate a list of names that contain 'term'
     current_name = [s.name for s in student_db if term in s.name]
 
-    print(f"\n--- Finding Students matching '{term}' ---")
+    print("\n--- Finding Students matching '{term}' ---")
+    print("Alert - Case Sensitive!\n")
 
+    # if the list doesnt contain the seach term
     if not current_name:
         print("No match found.\n")
         return 
     
-    
     print("---- Possible Names ---")
-    print("Alert - Case Sensitive!\n")
         
+    # find the names that are same with search term
     for student in student_db:
         for name in current_name:
             if student.name == name:
                 print(f"ID: {student.id}\nName: {student.name}\nInstructions: {student.enrolled_in}\n")
                 break
     
-#to find teacher by name and instrument
-#case insensitive
+# to find teacher by name and instrument
+# case insensitive
 def find_teachers(term, speciality_check):
-    #generate a list of names that 
-    matches = [t for t in teacher_db if term.lower() in t.name.lower()]
+    # generate a list of names that 
+    matches = [t for t in teacher_db if term in t.name.lower()]
 
     print(f"\n--- Finding Teachers matching '{term}' ---")
+    print(f"Alert - Case Insensitive!\n")
 
-    #teacher is not found
+    # teacher is not found
     if not matches:
         print("No match found.\n")
         return
@@ -199,49 +210,49 @@ def find_teachers(term, speciality_check):
     found_exact = False
 
     for teacher in matches:
+        # match teacher name and speciality
         if teacher.speciality.lower() == speciality_check.lower():
-            #match teacher name and speciality
             print(f"ID: {teacher.id}\nName: {teacher.name}\nSpeciality: {teacher.speciality}\n")
 
             found_exact = True
 
         else:
-            # Name matched, but speciality different
+            # name matched, but speciality different
             print(f"Do you mean...\nName: {teacher.name}\nSpeciality: {teacher.speciality}\n")
 
     if not found_exact:
         print("No exact speciality match found.\n")
 
 # --- Front Desk Functions ---
-#related to registration
+# related to registration
 def front_desk_register(name, instrument):
-    #variables
+    # variable
     global next_student_id
 
-    #create Student object, add to student_db, increment the ID.
+    # create Student object, add to student_db, increment the ID.
     new_student = Student(next_student_id, name)
     student_db.append(new_student)
     next_student_id += 1
     
-    #call front_desk_enrol() using the new student's ID and the provided instrument.
+    # call front_desk_enrol() using the new student's ID and the provided instrument.
     front_desk_enrol(new_student.id, instrument)
     print(f"Front Desk: Successfully registered '{name}' and enrolled them in '{instrument}'.")
 
 def find_student_by_id(student_id):
-    #loop student_db
-    #if ID matches student_id, return the student object.
+    # loop student_db
+    # if ID matches student_id, return the student object.
     for student in student_db:
         if student.id == student_id:
             return student
 
-    #return nothing if no record is found
+    # return nothing if no record is found
     return None
 
 def front_desk_enrol(student_id, instrument):
-    #Use find_student_by_id()
+    # use find_student_by_id()
     student = find_student_by_id(student_id)
 
-    #add the instrument to 'enrolled_in' list if found
+    # add the instrument to 'enrolled_in' list if found
     if student:
         student.enrolled_in.append(instrument)
 
@@ -250,27 +261,27 @@ def front_desk_enrol(student_id, instrument):
                 print(f"Front Desk: Enrolled student '{student.name}' in {student.enrolled_in}.")
 
     else:
-        #not found
+        # not found
         print(f"Error: Student ID {student_id} not found.")
 
-#related to find students
+# related to find students
 def front_desk_lookup(term, speciality_check) :
     
     print(f"\n--- Performing lookup for '{term}' ---")
 
-    #to add new student
+    # to add new student
     if len(speciality_check.split()) == 0:
         find_students(term)
 
-    #user does enter speciality
-    #to find teachers
+    # user does enter speciality
+    # to find teachers
     else:
-        find_teachers(term.title(), speciality_check)
+        find_teachers(term.title(), speciality_check.title())
 
 
 # --- Main Application ---
 def main(): 
-    #used in find_students/find_teachers
+    # used in find_students/find_teachers
     name_found = False
 
     while True:
@@ -283,19 +294,18 @@ def main():
         print("6. (Admin) List all Teachers")
         print("q. Quit")
 
-        #user input to choose operation
+        # user input to choose operation
         choice = input("Enter your choice: ")
         print()
 
-        #register new student
+        # register new student
         if choice == '1':
-            #direct to add_student function to add new students
+            # direct to add_student function to add new students
             name, instrument = add_student()
             front_desk_register(name, instrument)
 
-        #enrol existing student
+        # enrol existing student
         elif choice == '2':
-            # TODO: Prompt for student ID (as an int) and instrument, then call front_desk_enrol.
             try:
                 student_id = int(input("Enter student ID: "))
                 instrument = input("Enter instrument to enrol in: ").title()
@@ -303,9 +313,10 @@ def main():
             except ValueError:
                 print("Invalid ID. Please enter a number.")
 
-        #add new teacher
+        # add new teacher
         elif choice == "3":
             try:
+                # get input for name & speciality, remove after-text whitespace, and capitalize every first letter
                 name = input("Enter teacher name: ").rstrip().title()
                 speciality = input("Enter speciality: ").rstrip().title()
                 current_teacher_name = name.replace(" ","")
@@ -315,6 +326,7 @@ def main():
                     if letter in [1,2,3,4,5,6,7,8,9,0,"!","@","#","$","%","^","*","?","<",">"]:
                         raise ValueError
                 
+                # if user enters nothing
                 if len(current_teacher_name) == 0:
                     raise ValueError
                 
@@ -323,9 +335,8 @@ def main():
             except ValueError:
                 print("Please enter a valid name and speciality!")
 
-        #find student/teacher
+        # find student/teacher
         elif choice == '4':
-            # TODO: Prompt for a search term, then call front_desk_lookup.
             while name_found == False:
                 term = input("Enter search term (To exit, '999'): ")
                 speciality_check = input("Enter speciality (if teacher): ").title()
@@ -340,17 +351,20 @@ def main():
 
                 front_desk_lookup(term, speciality_check)
 
-        #view all students
+        # view all students
         elif choice == '5':
             trial = 1
             while True:
+                # only staff can access this feature
                 pw_trial = input("Please enter staff password: ")
 
+                # call list_student() if password is correct
                 if pw_trial == staff_pw:
                     list_students()
                     break
 
-                if trial == 3:
+                # lock the system when 3 incorrect password is entered
+                if trial >= 3:
                     print("System locked!")
 
                     while True:
@@ -364,17 +378,20 @@ def main():
                     print(f"{3-trial} trials left")
                     trial+=1
 
-        #view all teachers
+        # view all teachers
         elif choice == '6':
             trial = 1
 
             while True:
+                # only staff can access this feature
                 pw_trial = input("Please enter staff password: ")
 
+                # call list_student() if password is correct
                 if pw_trial == staff_pw:
                     list_teachers()
                     break
 
+                # lock the system when 3 incorrect password is entered
                 if trial == 3:
                     print("System locked!")
 
@@ -389,12 +406,12 @@ def main():
                     print(f"{3-trial} trials left")
                     trial+=1
 
-        #exit
+        # exit
         elif choice.lower() == 'q':
             print("Exiting program. Goodbye!")
             break
 
-        #invalid input
+        # invalid input
         else:
             print("Invalid choice. Please try again.")
 
@@ -408,4 +425,5 @@ if __name__ == "__main__":
     front_desk_register("Aidan Chiang","Guitar")
     front_desk_register("Aidan Lim","Bass")
 
+    # start the program
     main()
