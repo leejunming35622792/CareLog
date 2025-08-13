17a09cceaae132929faaea646e093cb80975f144
leejunming35622792 <jlee0465@student.monash.edu>
2025-08-13 20:36:17 +0800

    Data input functions from User

--- msms-project/pst2_main.py
+++ msms-project/pst2_main.py
@@ -9,6 +9,126 @@ import datetime
 DATA_FILE = "msms.json"
 app_data = {}
 
+
+
+# --- Data Input ---
+def get_studentName():
+    while True:
+        try:
+            student_name = input("Enter Student Name: ")
+
+            if student_name.replace(" ","").isalpha() != True:
+                raise ValueError
+            break
+        except:
+            print("Please enter a valid name!\n")
+        
+    return student_name.lstrip().rstrip().title()
+
+def get_studentID():
+    while True:
+        try:
+            student_id = input("Enter Student ID: ")
+            
+            if len(student_id) == 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Please enter a valid Student ID!\n")
+        
+    return student_id
+
+def get_studentCourse():
+    student_courses = []
+
+    while True:
+        try:
+            course_num = input("Number of course enrolled: ")
+
+            if len(course_num) == 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Please enter a valid number!\n")
+
+    for i in range(int(course_num)):
+        while True:
+            try:
+                student_course = input("Enter Course ID: ").upper().strip()
+                if len(student_course) == 0:
+                    raise ValueError
+                student_courses.append(student_course)
+                break
+            except ValueError:
+                print("Please enter a valid Course ID")
+    
+    return student_courses
+
+def get_courseID():
+    while True:
+        try:
+            course_id = input("Enter Course ID: ")
+            break
+        except:
+            print("Please enter a valid Course ID!\n")
+
+    return course_id
+
+def get_teacherName():
+    while True:
+        try:
+            teacher_name = input("Enter Teacher Name: ")
+
+            if teacher_name.replace(" ","").isalpha() != True:
+                raise ValueError
+            break
+        except:
+            print("Please enter a valid name!\n")
+        
+    return teacher_name.lstrip().rstrip().title()
+
+def get_teacherID():
+    while True:
+        try:
+            teacher_id = input("Enter Teacher ID: ")
+            if len(teacher_id) == 0:
+                raise ValueError
+            break
+        except ValueError:
+            print("Please enter a valid Teacher ID!\n")
+
+    return teacher_id
+
+def get_teacherSpeciality():
+    while True:
+        try:
+            teacher_speciality = input("Enter Speciality: ")
+            if len(teacher_speciality) == 0:
+                raise ValueError
+            break
+        except:
+            print("Please enter a valid Speciality!\n")
+
+    return teacher_speciality
+
+def get_teacherYear():
+    while True:
+        try:
+            year_joined = input("Enter Year Joined: ")
+
+            if int(year_joined) > 2025:
+                raise ValueError
+            
+            if len(year_joined) == 0:
+                raise ValueError
+            
+            teacher_year = 2025 - int(year_joined)
+            return teacher_year
+            
+        except ValueError:
+            print("Please enter a valid year!\n")
+ 
+ 
 # --- Core Persistence Engine ---
 def load_data(path=DATA_FILE):
     global app_data
