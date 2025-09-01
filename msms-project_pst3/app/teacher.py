cdca0bcd34cf997409f4ca9af6ae35a425f70dfa
leejunming35622792 <jlee0465@student.monash.edu>
2025-09-01 17:07:18 +0800

    Update teacher class

--- msms-project_pst3/app/teacher.py
+++ msms-project_pst3/app/teacher.py
@@ -4,17 +4,18 @@ class TeacherUser(User):
     """Represents a teacher."""
     # TODO: Implement the TeacherUser class, inheriting from User.
     # It should have an additional 'speciality' attribute in its __init__.
+    
     def __init__(self, user_id, name, speciality):
         super().__init__(user_id, name)
         self.speciality = speciality
 
 class Course:
     """Represents a single course offered by the school, linked to a teacher."""
-    def __init__(self, course_id, name, instrument, teacher_id):
+    def __init__(self, course_id, name, instrument, teacher_id, enrolled_student_ids, lessons):
         self.id = course_id
         self.name = name
         self.instrument = instrument
         self.teacher_id = teacher_id
-        # TODO: Initialize two empty lists: 'enrolled_student_ids' and 'lessons'.
-        self.enrolled_student_ids = []
-        self.lessons = [] # This will hold lesson dictionaries
+        self.enrolled_student_ids = enrolled_student_ids or [] # To store multiple student IDs
+        self.lessons = lessons or [] # To hold lesson dictionaries
+
