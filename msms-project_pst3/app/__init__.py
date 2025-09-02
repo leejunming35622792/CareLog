c8e0f8b293e2aee9039be9e13629cda8a4fa1a44
leejunming35622792 <jlee0465@student.monash.edu>
2025-09-02 11:03:14 +0800

    Create __init__.py file

---
+++ msms-project_pst3/app/__init__.py
@@ -0,0 +1,9 @@
+# Empty Python file
+# Act as a package
+# Used for import
+# For Python version of Python 3.3 below
+# Used for relative imports
+    # Absolute import: 
+        # from app.student import StudentUser
+    # Relative import:
+        # from .student import StudentUser
