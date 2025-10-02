3a2b340348ead939db12fb147a514bdee3caddb1
leejunming35622792 <jlee0465@student.monash.edu>
2025-10-02 16:30:31 +0800

    (gui) Add update profile for Patients

--- carelog/app/patient.py
+++ carelog/app/patient.py
@@ -1,18 +1,16 @@
-import datetime
-
 from app.user import User
 
 class PatientUser(User):
-    def __init__(self, p_id, username, password, name, gender, address, email, contact_num, date_joined):
+    def __init__(self, p_id, username, password, name, gender, address, email, contact_num, date_joined, p_record):
         self.p_id = p_id
         super().__init__(username, password, name, gender, address, email, contact_num, date_joined)
-        self.p_record = []
+        self.p_record = p_record or []
 
 class PatientRecord():
-    def __init__(self, pr_record_id, p_id, pr_conditions, pr_prediction_result, pr_confidence_score):
+    def __init__(self, pr_record_id, p_id, pr_timestamp, pr_conditions, pr_prediction_result, pr_confidence_score):
         self.pr_record_id = pr_record_id
         self.patient = p_id
-        self.pr_timestamp = datetime.datetime.now()
+        self.pr_timestamp = pr_timestamp
         self.pr_conditions = pr_conditions
         self.pr_prediction_result = pr_prediction_result
         self.pr_confidence_score = pr_confidence_score
