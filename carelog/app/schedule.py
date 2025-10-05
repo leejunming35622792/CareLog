dd0bebe3b1a680066050ca5fcb3291ea9723600b
Lim Seng Yang <slim0231@student.monash.edu>
2025-10-05 23:22:41 +0800

    Refactor: Moved record search function into record_manager

--- carelog/app/schedule.py
+++ carelog/app/schedule.py
@@ -127,23 +127,7 @@ class ScheduleManager():
         return len(self.doctors)
 
     # --- Patients ---
-    def search_record(self, p_id, record_id):
-        record_dict = {}
-        for record in self.records:
-            if record_id == record.pr_record_id:
-                record_dict = {
-                    "Record ID": record.pr_record_id,
-                    "Patient ID": record.p_id,
-                    "Date": record.pr_timestamp,
-                    "Conditions": ", ".join(record.pr_conditions.keys()),
-                    # "Conditions": ", ".join([f"{condition}: {severity}" for condition, severity in record.pr_conditions.items()]),
-                    "Medications": record.pr_medications,
-                    "Billings": record.pr_billings,
-                    "Prediction Result": record.pr_prediction_result,
-                    "Confidence Score": record.pr_confidence_score,
-                    "Remark": record.pr_remark,
-                }
-        return record_dict
+    
 
     def search_appt(self, chosen_appt_id):
         for appt in self.appointments:
